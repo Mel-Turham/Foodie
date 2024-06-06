ceaef255bb273fa50f06704f806d1798c7ebfb62
Mel/ <tchoikuemel06@gmail.com>
2024-06-06 01:18:20 +0100

    feacture added save data for localStorage and set body overflow hidden

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import { Contact, ErrorPage, Home, Menu, PlatDetails } from './pages';
-
+import { useEffect } from 'react';
+import { useToggleContext } from './Context/contextToggle';
+import useCartStore from './store/useCartStore';
 const App = () => {
+	const loadCart = useCartStore((state) => state.loadCart);
+
+	const { toggle } = useToggleContext();
+	useEffect(() => {
+		loadCart();
+	}, [loadCart]);
+	useEffect(() => {
+		const body = document.querySelector<HTMLBodyElement>('body')!;
+		if (toggle) {
+			body.classList.add('overflow-hidden');
+		} else {
+			body.classList.remove('overflow-hidden');
+		}
+	}, [toggle]);
+
 	return (
 		<Router>
 			<Routes>
