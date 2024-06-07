5ad08f7d8f4cb00ccacf38d8e7e91395a415a98d
Mel/ <tchoikuemel06@gmail.com>
2024-06-07 23:34:46 +0100

    Export Component as name export (Login, and register)

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,9 +2,8 @@ import Home from './Home/Home';
 import Contact from './Contacts/Contact';
 import Menu from './Menu/Menu';
 import ErrorPage from './NotFound/ErrorPage';
+import Login from './Auth/Login/Login';
 import PlatDetails from './PlatDetails/PlatDetails';
+import Register from './Auth/Register/Register';
 
-export { Home, Contact, Menu , ErrorPage, PlatDetails};
-
-
-
+export { Home, Contact, Menu, ErrorPage, PlatDetails, Login, Register };
