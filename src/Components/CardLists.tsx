af19174668e062a45bb0d3899168eab924b76039
Mel/ <tchoikuemel06@gmail.com>
2024-06-07 23:32:38 +0100

    Added animate side bars(framer-motion)

--- src/Components/CardLists.tsx
+++ src/Components/CardLists.tsx
@@ -3,17 +3,18 @@ import { IoCloseSharp } from 'react-icons/io5';
 import { FaMinus, FaPlus } from 'react-icons/fa6';
 import { useToggleContext } from '../Context/contextToggle';
 import useCartStore from '../store/useCartStore';
+import { motion } from 'framer-motion';
 
 const CardLists = () => {
 	const { setToggle } = useToggleContext();
 	const { cart, incrementQuantity, decrementQuantity, removeFromCart } =
 		useCartStore();
-    const total = useCartStore((state) => state.calculateTotal());
+	const total = useCartStore((state) => state.calculateTotal());
 
 	const cartItem = cart.map((item) => {
 		return (
 			<div
-				className='grid grid-cols-5 gap-2 place-content-center'
+				className='grid grid-cols-5 gap-2 pb-2 border-gray-500 solid border- border-b-1 place-content-center'
 				key={item.id}
 			>
 				<Image width={55} radius='none' height={55} src={item.image} />
@@ -63,11 +64,23 @@ const CardLists = () => {
 		);
 	});
 	return (
-		<section
-			onClick={() => setToggle(false)}
+		<motion.section
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ duration: 0.5 }}
 			className='fixed z-40 w-full h-screen bg-black/35 backdrop-blur-sm'
 		>
-			<div className='w-[35%] h-screen bg-white p-5'>
+			<motion.div
+				initial={{ x: '-300%', opacity: 0 }}
+				animate={{ x: 0, opacity: 1 }}
+				exit={{ x: '-300%', opacity: 0 }}
+				transition={{
+					duration: 0.6,
+					type: 'spring',
+					ease: 'easeInOut',
+				}}
+				className='w-[35%] h-screen bg-white p-5 overflow-y-scroll'
+			>
 				<div className='flex items-start justify-between'>
 					<h2 className='text-2xl font-semibold uppercase'>Foodie</h2>
 					<Button
@@ -96,19 +109,19 @@ const CardLists = () => {
 								Conectez vous!!
 							</Button>
 						</div>
-						<div className='flex items-center justify-between py-2 font-semibold capitalize border-black border-solid border-b-1'>
+						<div className='flex items-center justify-between py-2 font-semibold capitalize border-black border-solid border-b-1 '>
 							<p>image</p>
 							<p>Titre</p>
-							<p>Prix</p>
+							<p>Prix(Fcfa)</p>
 							<p>Qte</p>
 							<p>Remove</p>
 						</div>
 
-						<div className='flex flex-col gap-4 mt-4'>{cartItem}</div>
+						<div className='flex flex-col h-[550px] gap-4 mt-4'>{cartItem}</div>
 					</>
 				)}
-			</div>
-		</section>
+			</motion.div>
+		</motion.section>
 	);
 };
 export default CardLists;
